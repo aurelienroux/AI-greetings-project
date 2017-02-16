cf57acfed392338a0b56f82eea08cf6cf6aea9f1
Aurelien Roux <aureroux13@me.com>
2017-02-16 16:39:45 -0500

    input behavior logic

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -33,14 +33,40 @@ exports.handle = (client) => {
     }
   })
 
+  const handleGreeting = client.createStep({
+    satisfied(){
+      return false
+    },
+
+    prompt(){
+      client.addResponse('Hello world, I mean human')
+      client.done()
+    }
+  })
+
+  const handleGoodbye = client.createStep({
+    satisfied(){
+      return false
+    },
+
+    prompt(){
+      client.addResponse('See you later!')
+      client.done()
+    }
+  })
+
   client.runFlow({
     classifications: {
       // map inbound message classifications to names of streams
+      goodbye: 'goodbye',
+      greeting: 'greeting'
     },
     autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
+      goodbye: handleGoodbye,
+      greeting: handleGreeting,
       main: 'onboarding',
       onboarding: [sayHello],
       end: [untrained],
